3adb0f72a0df311658ab2cb89b39c631ab7cd912
Khalid-MK <khalidabdelnaser382@gmail.com>
2022-01-23 15:48:04 +0200

    .

--- gateway.js
+++ gateway.js
@@ -1,6 +1,7 @@
 // Load express
 const express = require('express');
 const helmet = require('helmet');
+const cors = require('cors');
 const app = express();
 
 // Load .env
@@ -14,6 +15,9 @@ app.use(helmet());
 // Load routes
 const routes = require('./routes');
 app.use('/', routes)
+
+// Enable CORS
+app.use(cors());
 
 app.listen(PORT, () => {
     console.log(`Gateway listening on ${PORT}`);
